07e44247e624ab9ed149783b6fefc4641d25c0f8
sahaana123 <96471924+sahaana123@users.noreply.github.com>
2022-07-19 18:09:41 +0530

    Add files via upload

---
+++ Game1sketch.js
@@ -0,0 +1,19 @@
+ var spacegreen
+ var robot
+ var robotrunning
+ var robotvillain
+ var fireball
+
+ function preload(){
+robot=loadAnimation("image robot","robot image running")
+robotvillain=loadImage("Robot villain")
+spacegreen=loadImage("image space green 1")
+fireball=loadImage("Fireball.png")
+}
+
+function setup(){
+
+}
+ function draw(){
+
+ }
